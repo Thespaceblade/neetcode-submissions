2ac7beba9bd7f7b02bd6560580611f7c87eb50a3
Jason Charwin <Jason.charwin360@gmail.com>
2026-05-17 16:05:05 -0400

    Add: counting-bits - submission-3

---
+++ Data Structures & Algorithms/counting-bits/submission-3.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] output = new int[n+1];
+        for (int i = 0; i <= n; i++){
+            output[i] = output[i / 2] + (i%2);
+
+        }
+        return output;
+    }
+}
